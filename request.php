37932be1b0c1502740cb429ee067c029c27f4df3
yaellizarraga <yaellizarraga@outlook.es>
2022-04-24 19:11:14 -0600

    Se nombra archivo a descargar como dice el PDF

--- request.php
+++ request.php
@@ -3,5 +3,5 @@ include "classRequest.php";
 
 $req = new Request;
 $url = "https://my-json-server.typicode.com/dp-danielortiz/dptest_jsonplaceholder/items";
-$file_name = "data.json";
+$file_name = "Respuesta1.json";
 $req->getRequest($url, $file_name, ['key' => 'color', 'val' => 'green']);
